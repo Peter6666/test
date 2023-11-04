4b7d487866386c51e6d5b7bb4be9bdd5a338a8f8
Peter6666 <2778500267@qq.com>
2023-11-04 07:51:38 +0000

    first commit

---
+++ Trainer.py
@@ -0,0 +1,8 @@
+[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = false
+	logallrefupdates = true
+[remote "origin"]
+	url = https://Peter6666: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@github.com/Peter6666/test.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
